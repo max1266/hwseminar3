9b128e35fc118fe8d9788ce822113569d6ceda13
Maxim Shmidt <history1266@mail.ru>
2022-12-05 15:17:27 +0700

    задание 19

---
+++ task19/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("введите число");
+string? n = Convert.ToString(Console.ReadLine());
+if((n[0] == n[4]) && (n[1] == n[3])){
+    Console.WriteLine("true");
+}
+else{
+    Console.WriteLine("false");
+}
